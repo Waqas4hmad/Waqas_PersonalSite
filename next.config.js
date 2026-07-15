888234f39ca874e68e4aefad6e7e3f994074129c
Waqas4hmad <waqa5.4hmad@gmail.com>
2026-07-15 22:10:52 +0100

    dd

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
-/** @type {import('next').NextConfig} */
+
 const nextConfig = {
-  reactStrictMode: true
-};
-
-module.exports = nextConfig;
-
-images: {
-  unoptimized: true
+  output: 'export', // This generates pure static files
+  trailingSlash: true,
+  images: { unoptimized: true } // Required for static hosting
 }
+
+module.exports = nextConfig
