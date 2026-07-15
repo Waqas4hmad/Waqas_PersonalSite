af0157bf8c797f553523844d51f46fd25274f973
Waqas4hmad <waqa5.4hmad@gmail.com>
2026-07-15 23:56:32 +0100

    icon changed

--- src/components/Layout/Page.tsx
+++ src/components/Layout/Page.tsx
@@ -2,7 +2,6 @@ import {NextPage} from 'next';
 import Head from 'next/head';
 import {useRouter} from 'next/router';
 import {memo, PropsWithChildren} from 'react';
-
 import {HomepageMeta} from '../../data/dataDef';
 
 const Page: NextPage<PropsWithChildren<HomepageMeta>> = memo(({children, title, description}) => {
@@ -17,9 +16,8 @@ const Page: NextPage<PropsWithChildren<HomepageMeta>> = memo(({children, title, 
         {/* several domains list the same content, make sure google knows we mean this one. */}
         <link href={`https://waqaswork.com${pathname}`} key="canonical" rel="canonical" />
 
-        {/* <link href="/favicon.ico" rel="icon" sizes="any" />
-        <link href="/icon.svg" rel="icon" type="image/svg+xml" /> */}
-        <link href="/apple-touch-icon.png" rel="apple-touch-icon" />
+         <link href="/favicon.ico" rel="icon" sizes="any" />
+        {/* <link href={Logo} rel="apple-touch-icon" /> */}
         <link href="/site.webmanifest" rel="manifest" />
 
         {/* Open Graph : https://ogp.me/ */}
